b2d6b7390d7bfd358e89b43c01ab1f1f38a8398c
HeeTaeKim996 <ttiiger1@naver.com>
2025-07-17 17:02:52 +0900

    123

--- _2_1_First/Resource.h
+++ _2_1_First/Resource.h
@@ -1,31 +1,28 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// _5_3_stringTable.rc에서 사용되고 있습니다.
+// _5_2_Menu_Cursor_Icon.rc에서 사용되고 있습니다.
 //
-#define IDR_MENU1						101
-#define IDR_MENU2						102
-
-#define ID_FILE_MENU2					1001
-#define ID_FILE_EXIT					1002
-#define ID_VIEW_LOVE					1003
-#define ID_VIEW_HARRY					1004
-#define ID_HARRY_LOVE					1005
-#define ID_BORI_FOOD					1006
-#define ID_SETTINGS_EXIT				1007
-
-#define IDI_ICON1						1101
-#define IDC_CURSOR1						1201
-
-#define IDR_ACCELERATOR1				1301
-#define IDR_ACCELERATOR2				1302
-
-#define IDR_STRING101					2001
+#define IDR_MENU1                       101
+#define IDR_MENU2                       102
+#define IDB_BITMAP1                     102
+#define ID_FILE_MENU2                   1001
+#define ID_FILE_EXIT                    1002
+#define ID_VIEW_LOVE                    1003
+#define ID_VIEW_HARRY                   1004
+#define ID_HARRY_LOVE                   1005
+#define ID_BORI_FOOD                    1006
+#define ID_SETTINGS_EXIT                1007
+#define IDI_ICON1                       1101
+#define IDC_CURSOR1                     1201
+#define IDR_ACCELERATOR1                1301
+#define IDR_ACCELERATOR2                1302
+#define IDR_STRING101                   2001
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
